058327181870e04d7ccadc6c21b6ec15240c12d9
harireddy7 <harimsd8@gmail.com>
2023-07-18 01:40:53 +0530

    fix: add repeat function examples in jsdoc

--- src/repeat/repeat.ts
+++ src/repeat/repeat.ts
@@ -3,6 +3,10 @@
  * @param {string} text text to repeat
  * @param {number} count repeat count
  * @returns {string} repeated string or empty string if count is less than 1
+ * @example
+ * repeat('*', 5) // *****
+ * repeat('cool', 3) // coolcoolcool
+ * repeat('hey', 0) // ''
  */
 const repeat = (text: string, count: number) => {
   if (!count || !text) return '';
